81b20d37942da60b5499dad7d015df8f491d7895
Hipjea <pierre.duverneix@gmail.com>
2022-09-22 18:01:38 +0200

    h5p-player

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,16 @@
-const init = () => {
-
-    
-
+const init = (cfg: any) => {
+    const search = setInterval(function() {
+        const element = document.querySelector('.h5p-player');
+        if (element) {
+            modalHandling(document.querySelector('.h5p-player'), cfg);
+            clearInterval(search);
+        }
+    }, 3000);
 };
+
+const modalHandling = (element: HTMLElement, cfg: any) => {
+    console.log(element, cfg);
+    element.style.display = 'none';
+}
 
 export { init };
