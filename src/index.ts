7100bd0e53c806833bcb0c69b51173198e77ad34
Hipjea <pierre.duverneix@gmail.com>
2022-09-23 15:18:50 +0200

    button fade in

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 // @ts-ignore
-import * as jQuery from 'jquery';
+import $ from 'jquery';
 // @ts-ignore
 import * as Str from 'core/str';
 import { Config } from './types';
@@ -38,26 +38,39 @@ const init = (config: Config) : void => {
     }, 3000);
 };
 
-const handleH5Pelement = (element: HTMLElement, config: Config) : void => {
+const handleH5Pelement = (h5pelement: HTMLElement, config: Config) : void => {
     createModal(config);
+    const $h5pelement = $(h5pelement) as JQuery;
 
-    const button = createElement('button', 'h5p-download', 'DOWNLOAD');
-    element.appendChild(button);
+    const $button = createButton('button', 'h5p-download', 'DOWNLOAD');
+    $h5pelement.append($button);
 
-    console.log(element);
+    // Fade the button in and out.
+    $h5pelement.on('mouseenter', () => {
+        $button.fadeIn();
+    }).on('mouseleave', () => {
+        $button.fadeOut();
+    });
 };
 
-const createElement = (type: string, classes: string, text?: string|null) : HTMLElement => {
+const createElement = (type: string, classes: string, text?: string|null) : JQuery => {
     const element = document.createElement(type) as HTMLElement;
     element.className = classes;
     element.innerHTML = text;
-    return element;
+    return $(element);
+}
+
+const createButton = (type: string, classes: string, text?: string|null) : JQuery => {
+    const element = document.createElement(type) as HTMLButtonElement;
+    element.className = classes;
+    element.innerHTML = text;
+    return $(element);
 }
 
 const createModal = (config: Config) : void => {
-    const modalOverlay = createElement('div', 'download-overlay');
-    const modal = createElement('div', 'download-modal', config.licenceIntro);
-    modalOverlay.appendChild(modal);
+    const $modalOverlay = createElement('div', 'download-overlay');
+    const $modal = createElement('div', 'download-modal', config.licenceIntro);
+    $modalOverlay.append($modal);
     // Append to the body
     //const body = document.querySelector('body') as HTMLBodyElement;
     //body.appendChild(modalOverlay);
