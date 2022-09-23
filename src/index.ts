67bcbc2dffd23e40fdaff1d62bae4fb92b2ba907
Hipjea <pierre.duverneix@gmail.com>
2022-09-23 12:22:07 +0200

    createElement

--- src/index.ts
+++ src/index.ts
@@ -2,26 +2,53 @@
 import * as jQuery from 'jquery';
 // @ts-ignore
 import * as Str from 'core/str';
+import { Config } from './types';
 
-const init = (cfg: any) => {
 
-    const search = setInterval(function() {
-        const element = document.querySelector('.h5p-player');
-        if (element) {
-            modalHandling(document.querySelector('.h5p-player'), cfg);
-            clearInterval(search);
-        }
-    }, 3000);
-
-};
-
-const modalHandling = (element: HTMLElement, cfg: any) => {
-    let testTxt = '';
+/**
+ * The script initialization function.
+ * 
+ * @param {Config} config
+ * @returns {void}
+ */
+const init = (config: Config): void => {
+    // Tests
+    console.log(config.backgroundColor);
     Str.get_string('content_reuse', 'filter_h5pdownload').done((str: string) => {
         console.log(str)
     });
-    console.log(element, cfg.textColor, testTxt);
-    element.style.display = 'none';
+
+    /**
+     * Search the H5P element within the document.
+     * 
+     * @param {function}
+     * @returns {number}
+     */
+    const searchElement: number = window.setInterval(() => {
+        const element = document.querySelector('.h5p-player') as HTMLIFrameElement;
+        if (element) {
+            handleH5Pelement(element, config);
+            clearInterval(searchElement);
+        }
+    }, 3000);
+};
+
+const handleH5Pelement = (element: HTMLIFrameElement, config: Config): void => {
+    const modalOverlay = createElement('div', 'download-overlay');
+    const modal = createElement('div', 'download-modal', config.licenceIntro);
+
+    modalOverlay.appendChild(modal);
+    const body = document.querySelector('body') as HTMLBodyElement;
+    body.appendChild(modalOverlay);
+
+    console.log(element);
+};
+
+const createElement = (type: string, classes: string, text?: string|null): HTMLElement => {
+    const element = document.createElement(type) as HTMLDivElement;
+    element.className = classes;
+    element.innerHTML = text;
+    return element;
 }
 
 export { init };
---
+++ src/types.d.ts
@@ -0,0 +1,10 @@
+export interface Config {
+    backgroundColor: string;
+    textColor: string;
+    licenceIntro: string;
+    licenceTarget: string;
+    licenceName: string;
+    licenceUrl: string;
+    reuseConditions: string;
+    isHVP: boolean;
+}
