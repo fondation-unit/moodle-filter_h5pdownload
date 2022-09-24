b49b2a9769f4b51f7b196ab718de92b7ed5c550d
Hipjea <pierre.duverneix@gmail.com>
2022-09-24 21:48:28 +0200

    comments

--- src/index.ts
+++ src/index.ts
@@ -17,7 +17,6 @@ const PLUGIN_NAME = 'filter_h5pdownload';
  * @returns {void}
  */
 const init = (config: Config) : void => {
-    // Tests
     Str.get_string('content_reuse', 'filter_h5pdownload').done((str: string) => config.modalTitle = str);
     Str.get_string('licence_to_use', 'filter_h5pdownload').done((str: string) => config.licenceToUse = str);
     Str.get_string('download', 'core').done((str: string) => config.downloadText = str);
@@ -44,10 +43,16 @@ const init = (config: Config) : void => {
     }, 3000);
 };
 
+/**
+ * Add the download button over the H5P element.
+ * 
+ * @param {HTMLElement} h5pelement
+ * @param {Config} config
+ * @returns {void}
+ */
 const handleH5Pelement = (h5pelement: HTMLElement, config: Config) : void => {
     const $h5pelement = $(h5pelement) as JQuery<HTMLElement>;
     config.downloadURL = getDownloadURL($(h5pelement));
-    console.log(config.downloadURL);
     const $button = createDownloadButton('button', 'h5p-download', config);
     $h5pelement.append($button);
 
@@ -59,6 +64,14 @@ const handleH5Pelement = (h5pelement: HTMLElement, config: Config) : void => {
     });
 };
 
+/**
+ * Create a new element.
+ * 
+ * @param {string} type
+ * @param {string} classes
+ * @param {strin|null} text
+ * @returns {JQuery<HTMLElement>}
+ */
 const createElement = (type: string, classes: string, text?: string|null) : JQuery<HTMLElement> => {
     const element = document.createElement(type) as HTMLElement;
     element.className = classes;
@@ -66,6 +79,15 @@ const createElement = (type: string, classes: string, text?: string|null) : JQue
     return $(element);
 }
 
+/**
+ * Create a new image element.
+ * 
+ * @param {string} title
+ * @param {string} classes
+ * @param {string|null} src
+ * @param {string|null} filename
+ * @returns {JQuery<HTMLElement>}
+ */
 const createImage = (title: string, classes: string, src?: string|null, filename?: string|null) : JQuery<HTMLElement> => {
     const $icon = $('<img/>');
     $icon.attr('alt', title)
@@ -78,6 +100,12 @@ const createImage = (title: string, classes: string, src?: string|null, filename
     return $icon;
 }
 
+/**
+ * Create the modal element.
+ * 
+ * @param {Config} config
+ * @returns {void}
+ */
 const createModal = (config: Config) : void => {
     // The modal surrounding overlay.
     const $modalOverlay = createElement('div', 'download-overlay');
@@ -125,6 +153,14 @@ const createModal = (config: Config) : void => {
     $(body).append($modalOverlay);
 }
 
+/**
+ * Create the download button.
+ * 
+ * @param {string} type:string
+ * @param {string} classes:string
+ * @param {Config} config:Config
+ * @returns {JQuery<HTMLElement>}
+ */
 const createDownloadButton = (type: string, classes: string, config: Config) : JQuery<HTMLElement> => {
     const element = document.createElement(type) as HTMLButtonElement;
     element.className = classes;
@@ -135,6 +171,12 @@ const createDownloadButton = (type: string, classes: string, config: Config) : J
     return $(element);
 }
 
+/**
+ * Retrieve the H5P file URL.
+ * 
+ * @param {JQuery<HTMLElement>} element
+ * @returns {string}
+ */
 const getDownloadURL = (element: JQuery<HTMLElement>) : string => {
     let src = element.find(".h5p-iframe").attr("src");
     if (src) {
@@ -152,4 +194,3 @@ const getDownloadURL = (element: JQuery<HTMLElement>) : string => {
 
 
 export { init };
-
